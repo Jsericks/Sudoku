104ee157040daa167c16ddde232432cfc7a27e6c
Scott Erickson <jsericks@gmail.com>
2013-02-10 13:32:34 -0700

    Inital CSS and HTML commit

--- Sudoku.js
+++ Sudoku.js
@@ -1,6 +1,15 @@
 /* 
 	Main function called for initialization of event listeners and the inital table
 */
-var main = function (){
+var main = function(){
+	
+}
+
+/* 
+	table generation using DOM scripting
+	form the html string and then return it to the 
+	div with id 'table'
+*/
+var tableGen = function(){
 	
 }
