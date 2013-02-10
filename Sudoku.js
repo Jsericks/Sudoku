8236f9ef6ce1e18ca3a87967585c0d701337b773
Scott Erickson <jsericks@gmail.com>
2013-02-10 14:32:37 -0700

    Inital javaScript commit

--- Sudoku.js
+++ Sudoku.js
@@ -1,7 +1,16 @@
 /* 
+	Sudoku table object
+*/
+function sudokuTable(){
+	this.table = new Array(9);
+	defaultTable = "040031069007000045030400000200000670800000003075000002000004080690000700720610090";
+} 
+
+/* 
 	Main function called for initialization of event listeners and the inital table
 */
-var main = function(){
+function main(){
+	gameTable = new sudokuTable;
 	
 }
 
@@ -10,6 +19,10 @@ var main = function(){
 	form the html string and then return it to the 
 	div with id 'table'
 */
-var tableGen = function(){
+function tableGen(){
+	var html = "<table>";
 	
+	var html += "</table";
+	
+	return html;
 }
